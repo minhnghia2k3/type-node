610e9c905dc2c4b2b025f0aa01ce3e0a04b95f50
minhnghia2k3 <BrozennVN@gmail.com>
2023-11-15 12:24:58 +0700

    Set up environment, express Server, setup Mongodb

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+// Web framework for nodejs
+import express from "express";
+import http from "http";
+// Parse incoming request `req.body`
+import bodyParser from "body-parser";
+// Parse cookie header and populate `req.cookies`
+import cookieParse from "cookie-parser";
+// Nodejs compression middleware: nén
+import compression from "compression";
+// Cross-origin resource sharing
+import cors from "cors";
+// Import mongoose -> ODM for mongodb
+import mongoose, { Error } from "mongoose";
+
+const app = express();
+app.use(cors({
+    credentials: true,
+}))
+
+app.use(compression());
+app.use(cookieParse());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log("Server running on http://localhost:8080/")
+})
+
+const MONGODB_URL = "mongodb+srv://brozennvn:Ng26012003@type-node.fsywdbl.mongodb.net/?retryWrites=true&w=majority"
+
+// init mongoose
+mongoose.connect(MONGODB_URL)
+    .then(() => {
+        console.log("Connected successfully to Mongodb")
+    })
+    .catch((error: Error) => {
+        console.log("Connect to Mongodb failed with error: ", error)
+    })
